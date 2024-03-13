6dd9832022b512fff1b3571265b588b186c46907
bgzdaniel <bgz.daniel@gmail.com>
2024-03-13 20:49:39 +0100

    overwork eval

--- eval.py
+++ eval.py
@@ -1,9 +1,10 @@
 import torch
 import numpy as np
 from utils.modelling_utils import init, get_answer
-from utils.eval_utils import Scorer, weighted_score
+from utils.eval_utils import Scorer, weighted_score, get_overall_scores, output_scores
 from langchain_community.vectorstores.chroma import Chroma
 import tensorflow as tf
+import pandas as pd
 
 def main():
     device = torch.device('cpu')
@@ -17,45 +18,42 @@ def main():
     eval(vectordb, chains, params, scorer)
 
 def eval(vectordb: Chroma, chains, params, scorer: Scorer):
+    df = pd.read_csv("eval_questions_handmade.csv").fillna("")
+    types = df["question_type"].unique()
     question_type_set = {}
+    for a_type in types:
+        questions_answers = df.loc[df["question_type"] == a_type][
+            ["generated_question", "generated_answer", "gpt_answer_without_context"]
+        ]
+        question_type_set[a_type] = questions_answers.to_dict(orient="records")
+
     question_types_scores = {}
+    question_types_scores_gpt = {}
     for question_type, qa_list in question_type_set.items():
         for qa in qa_list:
             question = qa["generated_question"]
             gold_answer = qa["generated_answer"]
+            gpt_answer = qa["gpt_answer_without_context"]
+
             predicted_answer = get_answer(question, "research", vectordb, chains, params)
+
             question_types_scores[question_type] = scorer.get_scores(
                 predictions=[predicted_answer], references=[gold_answer]
             )
+            question_types_scores_gpt[question_type] = scorer.get_scores(
+                predictions=[gpt_answer], references=[gold_answer]
+            )
 
     ranked_question_types_scores = dict(sorted(question_types_scores.items(), key=lambda x: weighted_score(x[1]), reverse=True))
+    ranked_question_types_scores_gpt = dict(sorted(question_types_scores_gpt.items(), key=lambda x: weighted_score(x[1]), reverse=True))
+    
+    overall_scores = get_overall_scores(ranked_question_types_scores)
+    overall_scores["weighted_score"] = weighted_score(overall_scores)
+    overall_scores_gpt = get_overall_scores(ranked_question_types_scores_gpt)
+    overall_scores_gpt["weighted_score"] = weighted_score(overall_scores_gpt)
 
-
-    overall_bleuscore = np.mean([scores["bleuscore"] for _, scores in ranked_question_types_scores.items()])
-    overall_rougescore = np.mean([scores["rougescore"] for _, scores in ranked_question_types_scores.items()])
-    overall_bertscore = np.mean([scores["bertscore"] for _, scores in ranked_question_types_scores.items()])
-    overall_bleurtscore = np.mean([scores["bleurtscore"] for _, scores in ranked_question_types_scores.items()])
-    overall_weighted_score = weighted_score(
-        {
-            "bleuscore": overall_bleuscore,
-            "rougescore": overall_rougescore,
-            "bertscore": overall_bertscore,
-            "bleurtscore": overall_bleurtscore,
-        })
-
-    with open("data/eval_results.txt", "w") as file:
-        for question_type, scores in ranked_question_types_scores.items():
-            file.write(f"{question_type}:\n")
-            for score_type, score in scores.items():
-                file.write(f"\t{score_type}: {score:.4f}\n")
-            file.write(f"weighted score: {weighted_score(scores):.4f}")
-            file.write("\n\n")
-        file.write(f"overall results:\n")
-        file.write(f"\tbleuscore: {overall_bleuscore:.4f}\n")
-        file.write(f"\trougescore: {overall_rougescore:.4f}\n")
-        file.write(f"\tbertscore: {overall_bertscore:.4f}\n")
-        file.write(f"\tbleurtscore: {overall_bleurtscore:.4f}\n")
-        file.write(f"\tweighted score: {overall_weighted_score:.4f}\n")
+    output_scores(ranked_question_types_scores, overall_scores, "eval_results_dbks.txt")
+    output_scores(ranked_question_types_scores_gpt, overall_scores_gpt, "eval_results_gpt.txt")
 
 if __name__ == '__main__':
     main()
--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -58,3 +58,30 @@ def weighted_score(scores):
         + scores["bleurtscore"] * 0.4
     )
     return weighted_score
+
+def get_overall_scores(ranked_question_types_scores):
+    overall_bleuscore = np.mean([scores["bleuscore"] for _, scores in ranked_question_types_scores.items()])
+    overall_rougescore = np.mean([scores["rougescore"] for _, scores in ranked_question_types_scores.items()])
+    overall_bertscore = np.mean([scores["bertscore"] for _, scores in ranked_question_types_scores.items()])
+    overall_bleurtscore = np.mean([scores["bleurtscore"] for _, scores in ranked_question_types_scores.items()])
+    return {
+            "bleuscore": overall_bleuscore,
+            "rougescore": overall_rougescore,
+            "bertscore": overall_bertscore,
+            "bleurtscore": overall_bleurtscore,
+        }
+
+def output_scores(ranked_question_types_scores, overall_scores, path):
+    with open(f"data/{path}", "w") as file:
+        for question_type, scores in ranked_question_types_scores.items():
+            file.write(f"{question_type}:\n")
+            for score_type, score in scores.items():
+                file.write(f"\t{score_type}: {score:.4f}\n")
+            file.write(f"weighted score: {weighted_score(scores):.4f}")
+            file.write("\n\n")
+        file.write(f"overall results:\n")
+        file.write(f"\tbleuscore: {overall_scores["bleuscore"]:.4f}\n")
+        file.write(f"\trougescore: {overall_scores["rougescore"]:.4f}\n")
+        file.write(f"\tbertscore: {overall_scores["bertscore"]:.4f}\n")
+        file.write(f"\tbleurtscore: {overall_scores["bleurtscore"]:.4f}\n")
+        file.write(f"\tweighted score: {overall_scores["weighted_score"]:.4f}\n")
